c5fef858169002e57f628319ac5c09f9e67b8cbc
Arlo <arlo.godfrey@outlook.com>
2022-12-27 12:56:08 -0600

    Update IDelegatable.cs

--- src/IDelegatable.cs
+++ src/IDelegatable.cs
@@ -1,7 +1,7 @@
 ﻿namespace OwlCore.ComponentModel
 {
     /// <summary>
-    /// Indicates that the class is holding a reference to an implementation of `<typeparamref name="T"/>`, to which public properties, events or methods may be delegated to.
+    /// Indicates that the class is holding a reference to an implementation of <typeparamref name="T"/>, to which public properties, events or methods may be delegated to.
     /// </summary>
     /// <typeparam name="T">The type that is delegated to.</typeparam>
     public interface IDelegatable<T>
